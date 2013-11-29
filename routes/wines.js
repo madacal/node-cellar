007540c559e16817d792aa76e230e32adf88efec
unknown <N822@A822.aknet.akb>
2013-11-29 11:08:53 +0200

    mongolab db fixed

--- routes/wines.js
+++ routes/wines.js
@@ -18,6 +18,7 @@ mongo.Db.connect(mongoUri, function (err, db) {
 */
 
 
+
 mongo.Db.connect(mongoUri, function (err, db) {
     if(!err) {
         console.log("Connected to 'winedatabase' database");
@@ -33,7 +34,7 @@ mongo.Db.connect(mongoUri, function (err, db) {
 exports.findById = function(req, res) {
     var id = req.params.id;
     console.log('Retrieving wine: ' + id);
-    db.collection('wines', function(err, collection) {
+    Db.collection('wines', function(err, collection) {
         collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
             res.send(item);
         });
@@ -41,7 +42,7 @@ exports.findById = function(req, res) {
 };
 
 exports.findAll = function(req, res) {
-    db.collection('wines', function(err, collection) {
+    Db.collection('wines', function(err, collection) {
         collection.find().toArray(function(err, items) {
             res.send(items);
         });
@@ -51,7 +52,7 @@ exports.findAll = function(req, res) {
 exports.addWine = function(req, res) {
     var wine = req.body;
     console.log('Adding wine: ' + JSON.stringify(wine));
-    db.collection('wines', function(err, collection) {
+    Db.collection('wines', function(err, collection) {
         collection.insert(wine, {safe:true}, function(err, result) {
             if (err) {
                 res.send({'error':'An error has occurred'});
@@ -69,7 +70,7 @@ exports.updateWine = function(req, res) {
     delete wine._id;
     console.log('Updating wine: ' + id);
     console.log(JSON.stringify(wine));
-    db.collection('wines', function(err, collection) {
+    Db.collection('wines', function(err, collection) {
         collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result) {
             if (err) {
                 console.log('Error updating wine: ' + err);
@@ -85,7 +86,7 @@ exports.updateWine = function(req, res) {
 exports.deleteWine = function(req, res) {
     var id = req.params.id;
     console.log('Deleting wine: ' + id);
-    db.collection('wines', function(err, collection) {
+    Db.collection('wines', function(err, collection) {
         collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
             if (err) {
                 res.send({'error':'An error has occurred - ' + err});
@@ -320,7 +321,7 @@ var populateDB = function() {
         picture: "waterbrook.jpg"
     }];
 
-    db.collection('wines', function(err, collection) {
+    Db.collection('wines', function(err, collection) {
         collection.insert(wines, {safe:true}, function(err, result) {});
     });
 
