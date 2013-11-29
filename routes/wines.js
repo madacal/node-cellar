f658b2f8881f057c550dff5d0a2050fbaf7f23c3
unknown <N822@A822.aknet.akb>
2013-11-29 11:01:01 +0200

    mongolab uri added

--- routes/wines.js
+++ routes/wines.js
@@ -4,10 +4,21 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/test'
+/*
 var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('winedatabase', server, {safe: true});
+db = new Db('winedatabase', server, { safe: true });
 
-db.open(function(err, db) {
+
+mongo.Db.connect(mongoUri, function (err, db) {
+    db.collection('winedatabase', function (er, collection) {
+        
+    });
+});
+*/
+
+
+mongo.Db.connect(mongoUri, function (err, db) {
     if(!err) {
         console.log("Connected to 'winedatabase' database");
         db.collection('wines', {safe:true}, function(err, collection) {
